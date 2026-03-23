e00d019caac5fa97c6651a5b23b174cd41eb20ee
yooshuu <yooshuu11@gmail.com>
2026-03-23 21:54:26 +0900

    Term Paper #2

--- src/main/java/Exam01.java
+++ src/main/java/Exam01.java
@@ -1,18 +1,15 @@
 import java.util.Scanner;
 
 public class Exam01 {
-    public static void main(String[] args){
+    public static void main(String[] args) {
         Scanner s = new Scanner(System.in);
 
-        int a, b;
-        double result;
+        int a = s.nextInt();
+        int b = s.nextInt();
 
-        a = s.nextInt();
-        b = s.nextInt();
+        double result = (double)a / b;
 
-        result = (double)a / b;
-
-        System.out.println(a + "/" + b + "=" + result);
+        System.out.println(a + " / " + b + " = " + result);
 
         s.close();
     }
