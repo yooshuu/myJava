6e9705be66ce336775e2e9691dc6326ce12e51ee
yooshuu <yooshuu11@gmail.com>
2026-03-27 15:07:05 +0900

    변형

---
+++ src/main/java/Code04_03.java
@@ -0,0 +1,12 @@
+
+public class Code04_03 {
+    public static void main(String[] args){
+        int n1 = 1;
+        int n2 = 2;
+
+        System.out.println(n1+n2);
+        System.out.println(n1-n2);
+        System.out.println(n1*n2);
+        System.out.println(n1/(double)n2);
+    }
+}
