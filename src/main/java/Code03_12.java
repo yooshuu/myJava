f92c2773d5af4e2b7bcfc0120c4d96d4f6351882
yooshuu <yooshuu11@gmail.com>
2026-03-27 14:34:43 +0900

    변형

--- src/main/java/Code03_12.java
+++ src/main/java/Code03_12.java
@@ -1,9 +1,15 @@
+import java.util.Scanner;
+
 public class Code03_12 {
     public static void main(String[] args){
-        int num = 99;
+        Scanner s = new Scanner(System.in);
+
+        int num = s.nextInt();
 
         System.out.println( (num > 100) && (num < 200));
         System.out.println( (num == 99) || (num == 100));
         System.out.println( !(num == 100));
+
+        s.close();
     }
 }
