6adef7f32db035dbd74db8b64d2b79aff7732d89
yooshuu <yooshuu11@gmail.com>
2026-03-31 10:34:32 +0900

    if 연습

---
+++ src/main/java/Code05_05.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class Code05_05 {
+    public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
+        int num = s.nextInt();
+
+        if (num%2 == 0) {
+            System.out.println("짝수입니다.");
+        }
+        else {
+            System.out.println("홀수입니다");
+        }
+    }
+}
