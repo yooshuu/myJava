b7cfcf08a38396223e05a8d26cc0092f6ab2b39c
yooshuu <yooshuu11@gmail.com>
2026-03-31 10:21:03 +0900

    if 연습

--- src/main/java/Code05_01.java
+++ src/main/java/Code05_01.java
@@ -1,6 +1,6 @@
 public class Code05_01 {
     public static void main(String[] args) {
-        int num = 99;
+        int num = 101;
 
         if (num < 100)
             System.out.println("100보다 작습니다.");
