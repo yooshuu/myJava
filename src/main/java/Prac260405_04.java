46c660fb99b0b3da441a62feb0013bfb5326aa5d
yooshuu <yooshuu11@gmail.com>
2026-04-05 17:56:28 +0900

    260405 실습문제4 연습

---
+++ src/main/java/Prac260405_04.java
@@ -0,0 +1,13 @@
+public class Prac260405_04 {
+    public static void main(String[] args) {
+        int i;
+
+        for (i=1 ; i<=20 ; i++) {
+            if (i%3 == 0) {
+                System.out.print("짝 ");
+            } else {
+                System.out.printf("%d ", i);
+            }
+        }
+    }
+}
