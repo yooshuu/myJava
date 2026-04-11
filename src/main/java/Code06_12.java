2f5c804280cf3d0c4d97a1a0c44617a5dd7835c3
yooshuu <yooshuu11@gmail.com>
2026-04-11 16:37:02 +0900

    연습

--- src/main/java/Code06_12.java
+++ src/main/java/Code06_12.java
@@ -9,11 +9,16 @@ public class Code06_12 {
         while (true) {
             System.out.print("숫자1 ==> ");
             num1 = s.nextInt();
+            if (num1 == 0)
+                break;
             System.out.print("숫자2 ==> ");
             num2 = s.nextInt();
 
             hap = num1 + num2;
             System.out.println(num1 + "+" + num2 + "=" + hap);
         }
+
+        System.out.println("0을 입력해서 계산을 종료합니다.");
+        s.close();
     }
 }
