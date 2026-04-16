2eb72004df6967181073774f58be7e02df67e708
yooshuu <yooshuu11@gmail.com>
2026-04-16 11:06:30 +0900

    Coding Test

--- src/main/java/Codetest0411_01.java
+++ src/main/java/Codetest0411_01.java
@@ -4,19 +4,19 @@ public class Codetest0411_01 {
     public static void main(String[] args) {
         Scanner s = new Scanner(System.in);
         System.out.print("행운의 숫자: ");
-        String num = s.next();
-        int num2 = Integer.parseInt(num);
+        String num_s = s.next();
+        int num = Integer.parseInt(num_s);
 
         System.out.print("금화의 무게: ");
-        String weight = s.next();
-        double weight2 = Double.parseDouble(weight);
+        String weight_s = s.next();
+        double weight = Double.parseDouble(weight_s);
 
         System.out.print("보유 포인트: ");
         int point = s.nextInt();
 
-        System.out.println("행운의 숫자 보상: " + num2*3);
-        System.out.printf("금화 무게 보상: %4.1f \n", weight2*3);
-        System.out.println("최종 포인트 보상: " + point*3);
+        System.out.printf("행운의 숫자 보상: %d \n", num*3);
+        System.out.printf("금화 무게 보상: %.1f \n", weight*3);
+        System.out.printf("최종 포인트 보상: %d \n", point*3);
 
         s.close();
     }
