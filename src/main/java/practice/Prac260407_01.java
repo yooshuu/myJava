2070597b6c075597aba51ca0455a91815e2144c2
yooshuu <yooshuu11@gmail.com>
2026-05-20 22:02:00 +0900

    패키지 경로 변경

---
+++ src/main/java/practice/Prac260407_01.java
@@ -0,0 +1,21 @@
+package practice;
+
+public class Prac260407_01 {
+    public static void main(String[] args) {
+        for (int i=1; i<=7; i++) {
+            int cnt;
+
+            if (i<=4) {
+                cnt = i;
+            } else {
+                cnt = 8-i;
+            }
+
+            for (int j=1; j<=cnt; j++) {
+                System.out.print("*");
+            }
+
+            System.out.println();
+        }
+    }
+}
