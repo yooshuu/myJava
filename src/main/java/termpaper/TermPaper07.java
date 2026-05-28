4d0055fddbba4f0331e4da1cb811c626d22b848a
yooshuu <yooshuu11@gmail.com>
2026-05-28 20:25:24 +0900

    ArrayList 2세대,3세대 for문

--- src/main/java/termpaper/TermPaper07.java
+++ src/main/java/termpaper/TermPaper07.java
@@ -5,21 +5,18 @@ import java.util.List;
 
 public class TermPaper07 {
     public static void main(String[] args) {
+        List<Integer> a = new ArrayList<>();
 
-        List<Integer> list = new ArrayList<>();
-
-        list.add(1);
-        list.add(2);
-        list.add(3);
-        list.add(4);
-        list.add(5);
+        for (int i=1; i<=5; i++) {
+            a.add(i);
+        }
 
         System.out.println("2세대 반복문");
-        for(Integer num : list) {
+        for (int num : a) {
             System.out.println(num);
         }
 
         System.out.println("3세대 반복문");
-        list.forEach(num -> System.out.println(num));
+        a.forEach(num -> System.out.println(num));
     }
 }
