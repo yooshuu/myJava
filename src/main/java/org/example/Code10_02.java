f12a211d2598efcbda8a73ed9ff95d6f4e309811
yooshuu <yooshuu11@gmail.com>
2026-05-29 18:11:05 +0900

    메서드 정의, 호출

---
+++ src/main/java/org/example/Code10_02.java
@@ -0,0 +1,20 @@
+package org.example;
+
+public class Code10_02 {
+    static void drawDice() {
+        int diceNum;
+        diceNum = (int) (Math.random() * 6 + 1);
+        System.out.println("결과 : " + diceNum);
+    }
+
+    public static void main(String[] args) {
+        System.out.println("A님, 주사위를 던집니다~");
+        drawDice();
+
+        System.out.println("B님, 주사위를 던집니다~");
+        drawDice();
+
+        System.out.println("C님, 주사위를 던집니다~");
+        drawDice();
+    }
+}
